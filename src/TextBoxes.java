75cdaca5eb8953d7040f27d8ca7d7b25f06da9e4
Simon Stueckemann <simon.stuckemann11@imperial.ak.uk>
2014-12-02 14:52:49 +0000

    Initial commit

---
+++ src/TextBoxes.java
@@ -0,0 +1,21 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+/**
+ * Created by Simon on 19/11/14.
+ */
+public class TextBoxes extends AnAction {
+
+    public TextBoxes() {
+        super("Text_Boxes");
+    }
+
+    public void actionPerformed(AnActionEvent e) {
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+        String txt = Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
+        Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+    }
+}
