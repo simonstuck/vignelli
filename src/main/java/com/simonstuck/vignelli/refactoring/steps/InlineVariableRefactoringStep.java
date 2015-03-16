281bafcaa403cc717c40caeda9d0cd3a5fea3dfa
Simon Stueckemann <simon.stuckemann11@imperial.ak.uk>
2015-03-16 17:03:37 +0000

    Keep track of affected statements when inlining.

--- src/main/java/com/simonstuck/vignelli/refactoring/steps/InlineVariableRefactoringStep.java
+++ src/main/java/com/simonstuck/vignelli/refactoring/steps/InlineVariableRefactoringStep.java
@@ -1,18 +1,25 @@
 package com.simonstuck.vignelli.refactoring.steps;
 
-import com.intellij.lang.refactoring.InlineHandler;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiLocalVariable;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.PsiStatement;
+import com.intellij.psi.search.searches.ReferencesSearch;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.refactoring.inline.InlineLocalHandler;
-import com.intellij.refactoring.util.InlineUtil;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class InlineVariableRefactoringStep implements RefactoringStep {
 
+
+    private static final Logger LOG = Logger.getInstance(InlineVariableRefactoringStep.class.getName());
     public static final String PROJECT_ARGUMENT_KEY = "project";
     public static final String VARIABLE_TO_INLINE_ARGUMENT_KEY = "variableToInline";
 
@@ -27,7 +34,25 @@ public class InlineVariableRefactoringStep implements RefactoringStep {
     @Override
     public Map<String, Object> process() {
         Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+        Collection<PsiStatement> affectedStatements = getAffectedStatements(variableToInline);
+
+        LOG.info("Statements affected by inline process: " + affectedStatements);
+        LOG.info("About to inline variable: " + variableToInline);
+
         InlineLocalHandler.invoke(project, editor, variableToInline, null);
-        return new HashMap<>();
+        Map<String, Object> results = new HashMap<>();
+        results.put("project", project);
+        results.put("inlineParents", affectedStatements);
+        return results;
+    }
+
+    private Collection<PsiStatement> getAffectedStatements(PsiLocalVariable variableToInline) {
+        Collection<PsiReference> references = ReferencesSearch.search(variableToInline).findAll();
+        Collection<PsiStatement> affectedStatements = new ArrayList<>(references.size());
+        for (PsiReference reference : references) {
+            PsiStatement statement = PsiTreeUtil.getParentOfType(reference.getElement(), PsiStatement.class);
+            affectedStatements.add(statement);
+        }
+        return affectedStatements;
     }
 }
