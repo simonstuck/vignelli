7644c2f1aa76609140a27bdf080a09d52c5821f4
Simon Stueckemann <simon.stuckemann11@imperial.ak.uk>
2015-04-14 19:30:53 +0200

    Remove unused graph sources.

--- src/main/java/com/simonstuck/vignelli/decomposition/graph/pdg/PDGControlDependence.java
+++
@@ -1,7 +0,0 @@
-package com.simonstuck.vignelli.decomposition.graph.pdg;
-
-public class PDGControlDependence extends PDGDependence {
-    public PDGControlDependence(PDGNode src, PDGNode dst) {
-        super(src, dst, PDGDependenceType.CONTROL);
-    }
-}
