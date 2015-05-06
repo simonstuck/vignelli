8aee902a2f15abbaaa777bf3f8d136094ff148cf
Simon Stueckemann <simon.stuckemann11@imperial.ak.uk>
2015-05-06 18:33:48 +0100

    Add documentation for class finder.

--- src/main/java/com/simonstuck/vignelli/psi/ClassFinder.java
+++ src/main/java/com/simonstuck/vignelli/psi/ClassFinder.java
@@ -3,6 +3,17 @@ package com.simonstuck.vignelli.psi;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.search.GlobalSearchScope;
 
+/**
+ * A {@link com.simonstuck.vignelli.psi.ClassFinder} instance allows clients to search for a
+ * given class name within some given scope.
+ */
 public interface ClassFinder {
+
+    /**
+     * Search the given search scope for the class given by the qualifiedName.
+     * @param searchScope The scope to search.
+     * @param qualifiedName The qualified name of the class to look for
+     * @return The class iff it is found, null otherwise.
+     */
     PsiClass findClass(GlobalSearchScope searchScope, String qualifiedName);
 }
--- src/main/java/com/simonstuck/vignelli/psi/IntelliJClassFinder.java
+++ src/main/java/com/simonstuck/vignelli/psi/IntelliJClassFinder.java
@@ -6,8 +6,13 @@ import com.intellij.psi.PsiClass;
 import com.intellij.psi.search.GlobalSearchScope;
 
 public class IntelliJClassFinder implements ClassFinder {
+    
     private final JavaPsiFacade javaPsiFacade;
 
+    /**
+     * Creates a default IntelliJ class finder, a wrapper around JavaPsiFacade
+     * @param project The project for which to create the facade.
+     */
     public IntelliJClassFinder(Project project) {
         javaPsiFacade = JavaPsiFacade.getInstance(project);
     }
